5fc8796392e26b20834dad2e6a3d152d47992383
Johannes Maier <johannes.maier@active-group.de>
2023-10-20 23:20:07 +0200

    Initialize cabal project: handshake + ready check

---
+++ src-exe/Main.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.Aeson qualified as Json
+import Data.Aeson.KeyMap qualified as KM
+import Data.Aeson ((.=))
+import Network.WebSockets
+import Control.Monad
+import System.IO (BufferMode (..), hSetBuffering, stdout)
+import Data.ByteString.Lazy qualified as LBS
+import System.Environment qualified as Env
+
+app conn = do
+  secret <- Env.getEnv "SERVER_SECRET"
+  putStrLn "connected!"
+
+  -- receive server handshake
+  msg <- receiveData conn
+  case Json.eitherDecode' msg of
+    Left err -> print err
+    Right (Json.Object o) -> do
+      putStrLn "\n"
+      LBS.putStr msg
+      let Just sessionId = KM.lookup "sessionId" o
+      putStrLn "\n"
+
+      -- send bot handshake
+      let botHandshake =
+            Json.object
+            [ "type" .= Json.String "BotHandshake"
+            , "sessionId" .= sessionId
+            , "name" .= Json.String "Ryu"
+            , "version" .= Json.String "0.1"
+            , "authors" .= Json.toJSON ["Johannes Maier" :: String]
+            , "secret" .= secret
+            ]
+      putStrLn $ "Sending handshake: " <> show (Json.encode botHandshake)
+      sendTextData conn $ Json.encode botHandshake
+
+      printNextMessage conn
+
+      let botReady =
+            Json.object [ "type" .= Json.String "BotReady" ]
+      sendTextData conn $ Json.encode botReady
+
+      printNextMessage conn
+
+printNextMessage conn = do
+      msg <- receiveData conn
+      putStrLn "\n"
+      LBS.putStr msg
+      putStrLn "\n"
+
+main :: IO ()
+main = do
+  hSetBuffering stdout LineBuffering
+  runClient "localhost" 7654 "/" app
