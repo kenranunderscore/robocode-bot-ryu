f66b82d2a292e5bc1654a633f4f245a603ad1234
Johannes Maier <johannes.maier@active-group.de>
2023-10-21 00:02:35 +0200

    Add a main loop
    For now just print out incoming events.

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
@@ -11,41 +13,58 @@ import Network.WebSockets
 import System.Environment qualified as Env
 import System.IO (BufferMode (..), hSetBuffering, stdout)
 
+app :: Connection -> IO b
 app conn = do
     secret <- Env.getEnv "SERVER_SECRET"
     putStrLn "connected!"
 
     -- receive server handshake
-    msg <- receiveData conn
-    case Json.eitherDecode' msg of
-        Left err -> print err
-        Right (Json.Object o) -> do
-            putStrLn "\n"
-            LBS.putStr msg
-            let Just sessionId = KM.lookup "sessionId" o
-            putStrLn "\n"
+    evt <- receiveEvent conn
+    putStrLn "\n"
+    print evt
+    let Just sessionId = KM.lookup "sessionId" evt.payload
+    putStrLn "\n"
 
-            -- send bot handshake
-            let botHandshake =
-                    Json.object
-                        [ "type" .= Json.String "BotHandshake"
-                        , "sessionId" .= sessionId
-                        , "name" .= Json.String "Ryu"
-                        , "version" .= Json.String "0.1"
-                        , "authors" .= Json.toJSON ["Johannes Maier" :: String]
-                        , "secret" .= secret
-                        ]
-            putStrLn $ "Sending handshake: " <> show (Json.encode botHandshake)
-            sendTextData conn $ Json.encode botHandshake
+    -- send bot handshake
+    let botHandshake =
+            Json.object
+                [ "type" .= Json.String "BotHandshake"
+                , "sessionId" .= sessionId
+                , "name" .= Json.String "Ryu"
+                , "version" .= Json.String "0.1"
+                , "authors" .= Json.toJSON ["Johannes Maier" :: String]
+                , "secret" .= secret
+                ]
+    putStrLn $ "Sending handshake: " <> show (Json.encode botHandshake)
+    sendTextData conn $ Json.encode botHandshake
 
-            printNextMessage conn
+    printNextMessage conn
 
-            let botReady =
-                    Json.object ["type" .= Json.String "BotReady"]
-            sendTextData conn $ Json.encode botReady
+    let botReady =
+            Json.object ["type" .= Json.String "BotReady"]
+    sendTextData conn $ Json.encode botReady
 
-            printNextMessage conn
+    mainLoop conn
 
+mainLoop :: Connection -> IO b
+mainLoop conn = forever $ do
+    evt <- receiveEvent conn
+    print evt
+
+newtype Event = Event {payload :: KM.KeyMap Json.Value}
+    deriving newtype (Show, Json.FromJSON)
+
+receiveEvent :: Connection -> IO Event
+receiveEvent conn = do
+    raw <- receiveData conn
+    case Json.eitherDecode' raw of
+        Left err -> fail $ show err
+        Right (Json.Object o) ->
+            pure $ Event o
+        Right unexpected ->
+            fail $ "Unexpected event JSON: " <> show unexpected
+
+printNextMessage :: Connection -> IO ()
 printNextMessage conn = do
     msg <- receiveData conn
     putStrLn "\n"
