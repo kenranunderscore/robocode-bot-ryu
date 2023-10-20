704f274d6d89ef68894b7c2d09be1f05b9072a49
Johannes Maier <johannes.maier@active-group.de>
2023-10-20 23:32:06 +0200

    flake: refactor and add tooling

--- flake.nix
+++ flake.nix
@@ -1,11 +1,23 @@
 {
-  description = "A very basic flake";
+  description = "Development environment for Ryu, a Robocode Tank Royale bot";
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
-      pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.cabal-install pkgs.haskell.compiler.ghc94 pkgs.zlib ];
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import inputs.nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.haskell.packages.ghc94.haskell-language-server
+            pkgs.cabal-install
+            pkgs.haskell.compiler.ghc94
+            pkgs.zlib
+            pkgs.nixfmt
+          ];
+        };
+      });
 }
